922958df6cd0df5714d905ad69c97e50d6fb54d2
samgo23 <gordsam18@gmail.com>
2022-11-25 13:57:45 -0700

    points update

--- greed/game/casting/artifact.py
+++ greed/game/casting/artifact.py
@@ -4,33 +4,25 @@ from game.casting.actor import Actor
 class Artifact(Actor):
 
     def __init__(self):
+        """Contructs a new artifact.
+        
+        Arg: 
+            self._points: Keeps track of the point total.
+        """
         super().__init__()
-        self._message = ''
+        self._points = 0
     
-    ''' def get_message(self):
-        """Gets the artifact's message.
-        
-        Returns:
-            string: The artifact's message.
-        """
-        return self._message
     
-    def set_message(self, message):
-        """Updates the text to the given value.
-        
-        Args:
-            text (string): The given value.
-        """
-        self._message = message
-    '''
     def calculate_points(self):
-        points = 0
+        """Calculates user point total.
+        
+        """
 
         if (self.get_text() == '*'):
-            points = 1
+            self._points = 1
         
         else:
-            points = -1
+            self._points = -1
 
-        return points
+        return self._points
     
